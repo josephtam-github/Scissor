00749305e86c16cee40693f93f2957a11f3724f7
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-14 22:53:22 +0100

    feat: create database model for links

---
+++ api/v1/models/links.py
@@ -0,0 +1,29 @@
+from api.v1 import db
+from datetime import datetime
+
+
+class Link(db.Model):
+    __tablename__ = "link"
+    link_id = db.Column(db.Integer(), primary_key=True)
+    original_link = db.Column(db.String(50), nullable=False, unique=True)
+    custom_link = db.Column(db.String(45), nullable=False)
+    short_link = db.Column(db.String(45), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return f"<User {self.user_id}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    @classmethod
+    def get_by_id(cls, user_id):
+        return cls.query.get_or_404(user_id)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
