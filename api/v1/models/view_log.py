809da553c2e3204c5e56ab5e029a3d055a754b1b
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-14 23:42:31 +0100

    feat: create database model for link view

---
+++ api/v1/models/view_log.py
@@ -0,0 +1,28 @@
+from api.v1 import db
+from datetime import datetime
+
+
+class ViewLog(db.Model):
+    __tablename__ = "viewlog"
+    view_log_id = db.Column(db.Integer(), primary_key=True)
+    link_id = db.Column(db.Integer(), db.ForeignKey("link.link_id"), nullable=False)
+    ip_address = db.Column(db.String(50), nullable=False, unique=True)
+    viewed_on = db.Column(db.DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return f"<User {self.user_id}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    @classmethod
+    def get_by_id(cls, user_id):
+        return cls.query.get_or_404(user_id)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
