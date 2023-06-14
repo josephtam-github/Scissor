311746ba7a67881eeca75d7ed813bdede4bbfbd2
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-14 21:02:19 +0100

    feat: instantiate and config smorest api

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_smorest import Api
 from .config.config import config_dict
 from .utils import db, ma
 
@@ -12,5 +13,17 @@ def create_app(config=config_dict['test']):
     db.init_app(app)
     ma.init_app(app)
     migrate = Migrate(app, db)
+
+    app.config["API_TITLE"] = "Scissor"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.2"
+    app.config["API_SPEC_OPTIONS"] = {
+        "description": 'A flask-smorest API that allows users to shorten url links'}
+    app.config['OPENAPI_URL_PREFIX'] = '/'
+    app.config['OPENAPI_JSON_PATH'] = 'api-spec.json'
+    app.config['OPENAPI_SWAGGER_UI_PATH'] = '/'
+    app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.25.x/'
+
+    api = Api(app)
 
     return app
