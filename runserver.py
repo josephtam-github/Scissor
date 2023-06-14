59d6a6660b9cdfdc6b14076c67af477f0c32bd2a
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-14 21:13:22 +0100

    feat: run flask app from create_app

--- runserver.py
+++ runserver.py
@@ -1,12 +1,9 @@
-from flask import Flask
+from api.v1 import create_app, db
+from api.v1.config.config import config_dict
 
-app = Flask(__name__)
+app = create_app(config=config_dict['prod'])
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
