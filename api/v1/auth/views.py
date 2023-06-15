ba4297b24795178c19d9e4dcecd786fb9deeecec
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-15 02:39:19 +0100

    feat: create user registration view

--- api/v1/auth/views.py
+++ api/v1/auth/views.py
@@ -12,3 +12,30 @@ auth = Blueprint(
     url_prefix='/auth',
     description='Authentication for user registration and login with JWT token'
 )
+
+
+@auth.route('/register')
+class Register(MethodView):
+    @auth.arguments(UserSchema)
+    @auth.response(HTTPStatus.CREATED, UserSchema, description='Returns an object containing '
+                                                               'the created user\'s detail')
+    def post(self, new_data):
+        """Register a new user
+
+        Returns the new user info from the database
+        """
+        # Check if username and email exists
+        email_exist = User.query.filter_by(email=new_data['email']).first()
+        if email_exist:
+            abort(HTTPStatus.NOT_ACCEPTABLE, message='This email already exists')
+        else:
+            new_user = User(
+                username=new_data['username'],
+                firstname=new_data['firstname'],
+                lastname=new_data['lastname'],
+                email=new_data['email'],
+                password_hash=generate_password_hash(new_data['password']),
+            )
+            new_user.save()
+
+        return new_user, HTTPStatus.CREATED
