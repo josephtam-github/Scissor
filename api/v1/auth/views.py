5c596bd05ef997bcd9cf511794cfb17f663f203b
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-15 03:09:55 +0100

    feat: create login views and jwt tokens

--- api/v1/auth/views.py
+++ api/v1/auth/views.py
@@ -4,7 +4,9 @@ from ..models.users import User
 from ..models.schemas import UserSchema, LinkSchema, ViewCountSchema, ViewLogSchema
 from http import HTTPStatus
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_jwt_extended import create_access_token,create_refresh_token, jwt_required, \
+    get_jwt_identity, get_jwt
+from flask import jsonify
 
 auth = Blueprint(
     'Auth',
@@ -39,3 +41,38 @@ class Register(MethodView):
             new_user.save()
 
         return new_user, HTTPStatus.CREATED
+
+
+@auth.route('/login')
+class Login(MethodView):
+    @auth.arguments(UserSchema)
+    @auth.response(HTTPStatus.ACCEPTED, UserSchema, description='Returns the access and return tokens')
+    def post(self, login_data):
+        """Logs in user
+
+        Returns access and refresh tokens
+        """
+
+        if 'email' in login_data.keys():
+            email = login_data['email']
+            user = User.query.filter_by(email=email).first()
+
+        elif 'username' in login_data.keys():
+            username = login_data['username']
+            user = User.query.filter_by(username=username).first()
+        else:
+            abort(HTTPStatus.BAD_REQUEST, message='You must input either your username or your email '
+                                                  'along with your password')
+        password = login_data['password']
+
+        if (user is not None) and check_password_hash(user.password_hash, password):
+            access_token = create_access_token(identity=user.user_id)
+            refresh_token = create_refresh_token(identity=user.user_id)
+            response = {
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }
+            return jsonify(response), HTTPStatus.ACCEPTED
+        else:
+            abort(HTTPStatus.UNAUTHORIZED, message='Invalid credentials')
+
