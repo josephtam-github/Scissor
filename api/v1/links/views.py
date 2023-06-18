49f217747a85a3388d0de32d774f1ff68e0294d6
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-18 05:12:08 +0100

    feat: create method view for expanding shortened url

--- api/v1/links/views.py
+++ api/v1/links/views.py
@@ -1,4 +1,5 @@
 from sqlalchemy import func, or_
+from flask import redirect
 from flask_smorest import Blueprint, abort
 from flask.views import MethodView
 from ..models.links import Link
@@ -57,5 +58,30 @@ class Shorten(MethodView):
             new_link.save()
 
             return new_link, HTTPStatus.CREATED
+        else:
+            abort(HTTPStatus.INTERNAL_SERVER_ERROR, message='Internal server error please try again later')
+
+
+@short_link.route('/<string:short_link_code>')
+class Expand(MethodView):
+
+    @true_link.response(HTTPStatus.OK, description='Returns an object containing the true link of shortened URL')
+    def get(self, short_link_code):
+        """Returns true link of shortened URL
+
+
+        Turns short-link code to link ID, looks up the true-link with the ID, then returns true link
+        """
+
+        link_id = url2id(short_link_code)
+
+        if not link_id:
+            return abort(HTTPStatus.NOT_ACCEPTABLE, message='This short link is invalid')
+        elif Link is not None:
+            result_link = Link.query.filter_by(link_id=link_id).first()
+            if result_link:
+                return jsonify(result_link.true_link)
+            else:
+                return abort(HTTPStatus.NOT_FOUND, message='Can\'t find original link')
         else:
             abort(HTTPStatus.INTERNAL_SERVER_ERROR, message='Internal server error please try again later')
