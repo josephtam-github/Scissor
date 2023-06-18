8aef5e1acc1bdcd4708d5d1ead517553d6713aa1
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-18 04:57:31 +0100

    fix: remove unique constraint from user_id column in link table

--- api/v1/models/links.py
+++ api/v1/models/links.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Link(db.Model):
     __tablename__ = "link"
     link_id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.user_id'), unique=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.user_id'))
     true_link = db.Column(db.String(50), nullable=False, unique=True)
     custom_link = db.Column(db.String(45), nullable=False)
     short_link = db.Column(db.String(45), nullable=False)
