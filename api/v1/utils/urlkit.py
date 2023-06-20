d3c0e6124951c1fb3dcc3c2154c7bba16f66f258
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-06-20 05:13:23 +0100

    feat: create URL validation function

--- api/v1/utils/urlkit.py
+++ api/v1/utils/urlkit.py
@@ -1,4 +1,45 @@
-from random import randrange
+import re
+import urllib
+
+
+def validate_url(url: str):
+    # Gotten from https://stackoverflow.com/a/55827638/15600487
+    # Check https://regex101.com/r/A326u1/5 for reference
+    DOMAIN_FORMAT = re.compile(
+        r"(?:^(\w{1,255}):(.{1,255})@|^)"  # http basic authentication [optional]
+        r"(?:(?:(?=\S{0,253}(?:$|:))"  # check full domain length to be less than or equal to 253 (starting after http basic auth, stopping before port)
+        r"((?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+"  # check for at least one subdomain (maximum length per subdomain: 63 characters), dashes in between allowed
+        r"(?:[a-z0-9]{1,63})))"  # check for top level domain, no dashes allowed
+        r"|localhost)"  # accept also "localhost" only
+        r"(:\d{1,5})?",  # port [optional]
+        re.IGNORECASE
+    )
+    SCHEME_FORMAT = re.compile(
+        r"^(http|hxxp|ftp|fxp)s?$",  # scheme: http(s) or ftp(s)
+        re.IGNORECASE
+    )
+
+    url = url.strip()
+
+    result = urllib.parse.urlparse(url)
+    scheme = result.scheme
+    domain = result.netloc
+
+    if not url:
+        raise Exception("No URL specified")
+    elif len(url) > 2048:
+        raise Exception("URL exceeds its maximum length of 2048 characters (given length={})".format(len(url)))
+    elif not scheme:
+        raise Exception("No URL scheme specified")
+    elif not re.fullmatch(SCHEME_FORMAT, scheme):
+        raise Exception("URL scheme must either be http(s) or ftp(s) (given scheme={})".format(scheme))
+    elif not domain:
+        raise Exception("No URL domain specified")
+    elif not re.fullmatch(DOMAIN_FORMAT, domain):
+        raise Exception("URL domain malformed (domain={})".format(domain))
+
+    return url
+
 
 salt = 3901
 
